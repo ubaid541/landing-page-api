b27d95b9acb55b79fd4f3da3776ebd66b4bf2ac0
ubaidawan <malikunknown633@gmail.com>
2024-04-26 11:18:22 +0500

    queries added to create tables

--- index.js
+++ index.js
@@ -124,6 +124,68 @@ app.get("/api/landingPage", (req, res) => {
   fetchLandingPage(con, domain, res);
 });
 
+app.get("/", (req, res) => {
+  const createPageSettingsTableQuery = `
+  CREATE TABLE page_settings (
+    id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    page_id INT(11) NOT NULL,
+    domain VARCHAR(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+    path_url VARCHAR(191) COLLATE utf8mb4_unicode_ci,
+    header_code VARCHAR(191) COLLATE utf8mb4_unicode_ci,
+    body_code VARCHAR(191) COLLATE utf8mb4_unicode_ci,
+    footer_code VARCHAR(191) COLLATE utf8mb4_unicode_ci,
+    added_by INT(11) NOT NULL,
+    added_by_name VARCHAR(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+    updated_by INT(11),
+    updated_by_name VARCHAR(191) COLLATE utf8mb4_unicode_ci,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )
+`;
+
+  con.query(createPageSettingsTableQuery, function (err, results) {
+    if (err) {
+      console.error("Error creating page_settings table:", err);
+      return res.status(400).json({
+        error: err,
+        message: "Error occured while creating page settings table.",
+      });
+    } else {
+      console.log("Page_settings table created successfully");
+      return res.status(200).json({ message: "Page settings table created" });
+    }
+  });
+
+  // Run the query to create the page_templates table
+  const createPageTemplatesTableQuery = `
+  CREATE TABLE page_templates (
+    id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    template_name VARCHAR(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+    template_type VARCHAR(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+    html LONGTEXT COLLATE utf8mb4_general_ci,
+    css LONGTEXT COLLATE utf8mb4_general_ci,
+    added_by INT(11),
+    added_by_name VARCHAR(191) COLLATE utf8mb4_unicode_ci,
+    updated_by INT(11),
+    updated_by_name VARCHAR(191) COLLATE utf8mb4_unicode_ci,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )
+`;
+
+  con.query(createPageTemplatesTableQuery, function (err, results) {
+    if (err) {
+      console.error("Error creating page_templates table:", err);
+      return res.status(400).json({
+        error: err,
+        message: "Error occured while creating page templates table.",
+      });
+    } else {
+      console.log("Page_templates table created successfully");
+    }
+  });
+});
+
 app.listen(2000, () => {
   console.log("server running on port 2000.");
 });
