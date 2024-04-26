9d81fe2d2e10f1e4a8043ba2d32ded86f47b9f64
ubaidawan <malikunknown633@gmail.com>
2024-04-26 10:55:11 +0500

    fetched landing page based on domain

--- controller/index.js
+++ controller/index.js
@@ -1,13 +1,48 @@
-import { injectHeadAndCSS } from "../utils/InjectionFunction";
-const fetchLandingPage = (con, domain) => {
-  const query = `SELECT pt.*, ps.*
-  FROM page_templates pt
-  JOIN page_settings ps ON pt.id = ps.page_id
-  WHERE pt.id = 52;
-`;
-  con.query(query, function (err, result, fields) {
-    if (err) throw err;
-    console.log("landing pages :: ", result);
+import { decompressData } from "../utils/Decompression.js";
+import { injectHeadAndCSS } from "../utils/InjectionFunction.js";
+const fetchLandingPage = (con, domain, res) => {
+  const checkDomain = `SELECT * from page_settings where domain = '${domain}'`;
+  //   const query = `SELECT pt.*, ps.*
+  //   FROM page_templates pt
+  //   JOIN page_settings ps ON pt.id = ps.page_id
+  //   WHERE pt.id = 52;
+  // `;
+  con.query(checkDomain, function (err, result) {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: err, message: "Internal server error" });
+    }
+    if (result.length > 0) {
+      console.log("domain found:: ", result);
+
+      const fetchLandingPage = `SELECT ps.*, pt.*
+         FROM page_settings ps
+         JOIN page_templates pt ON ps.page_id = pt.id
+         WHERE domain = ?; `;
+      con.query(fetchLandingPage, [domain], function (err, dataResult) {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            error: err,
+            message: "Error occured while fetching landing page.",
+          });
+        }
+
+        console.log("landing page found:: ", dataResult);
+        const html = decompressData(dataResult[0]?.html);
+        const css = decompressData(dataResult[0]?.css);
+
+        const renderHTML = injectHeadAndCSS(html, css);
+        return res.status(200).send(renderHTML);
+      });
+    } else {
+      console.log("no domains found: ", result);
+      res
+        .status(404)
+        .json({ data: result, message: "No data found with this domain" });
+    }
   });
 };
 
--- index.js
+++ index.js
@@ -59,22 +59,22 @@ if (con.state !== "connected" && con.state === "disconnected") {
 }
 
 // Function to inject `<head>` tag and CSS (replace if using a templating engine)
-function injectHeadAndCSS(content, cssContent) {
-  const headStart = content.indexOf("<body");
-  if (headStart !== -1) {
-    const styledHTML =
-      content.slice(0, headStart) +
-      "<head><style>" +
-      cssContent +
-      "</style></head>" +
-      content.slice(headStart);
-    return styledHTML;
-  } else {
-    // Handle case where no `<body>` tag is found
-    console.warn("`<body>` tag not found in HTML content.");
-    return content; // Fallback to unstyled content
-  }
-}
+// function injectHeadAndCSS(content, cssContent) {
+//   const headStart = content.indexOf("<body");
+//   if (headStart !== -1) {
+//     const styledHTML =
+//       content.slice(0, headStart) +
+//       "<head><style>" +
+//       cssContent +
+//       "</style></head>" +
+//       content.slice(headStart);
+//     return styledHTML;
+//   } else {
+//     // Handle case where no `<body>` tag is found
+//     console.warn("`<body>` tag not found in HTML content.");
+//     return content; // Fallback to unstyled content
+//   }
+// }
 
 // app.get("/api/domain", async (req, res) => {
 //   // Make the function async
@@ -121,7 +121,7 @@ app.get("/api/landingPage", (req, res) => {
   const domain = req.hostname;
   console.log("domain:: ", domain);
 
-  fetchLandingPage(con, domain);
+  fetchLandingPage(con, domain, res);
 });
 
 app.listen(2000, () => {
